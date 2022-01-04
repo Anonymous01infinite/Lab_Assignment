6b79851c7062c311bfccb62e85743a591ea94751
Abhijeet Kumar Singh <85782092+Anonymous01infinite@users.noreply.github.com>
2022-01-04 10:01:07 +0530

    Lab Assignment 8
    Solution of lab assignment 8

---
+++ Lab 8/Question2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pen pen = new Pen();
+            pen.StartWriting();
+            pen.StopWriting();
+
+            Console.ReadLine();
+        }
+    }
+    sealed class Pen
+    {
+        public void StartWriting()
+        {
+            Console.WriteLine("Start writing");
+        }
+        public void StopWriting()
+        {
+            Console.WriteLine("Stop writing");
+        }
+    }
+}
